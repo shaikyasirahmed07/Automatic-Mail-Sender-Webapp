b9a47f89f299c71fee778379e3c71ad6ae7fe526
SHAIK YASIR AHMED <shaikyasirahmed07@SHAIKs-MacBook-Air-3.local>
2025-04-10 22:41:42 +0530

    Updated project with latest changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import * as THREE from 'three';
+import NET from 'vanta/dist/vanta.net.min';
 import EmailForm from './components/EmailForm';
 import './App.css';
 
 function App() {
+  const vantaRef = useRef(null);
+
+  useEffect(() => {
+    const effect = NET({
+      el: vantaRef.current,
+      THREE,
+      color: 0x8b5cf6,
+      backgroundColor: 0x000000,
+      points: 10.0,
+      maxDistance: 20.0,
+      spacing: 15.0,
+    });
+    return () => {
+      effect && effect.destroy();
+    };
+  }, []);
+
   return (
-    <div className="App">
+    <div className="App" ref={vantaRef}>
       <EmailForm />
     </div>
   );
